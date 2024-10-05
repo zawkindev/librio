170a37d9b62fb68431fedc0d9a0c8e86f5fa4ba4
zawkindev <zawkindev@gmail.com>
2024-10-05 12:12:22 +0500

    feat: book handler

---
+++ src/main/java/com/example/demo/BookController.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @PostMapping("/book/")
+    public Boolean create(@RequestBody BookModel book) {
+        return true;
+    }
+
+    @GetMapping("/book/")
+    public List<BookModel> getBooks() {
+        return bookService.getBookList();
+    }
+
+    @GetMapping("/book/{id}")
+    public BookModel getById(@PathVariable("id") String id) {
+        return bookService.getById(id);
+    }
+
+    @DeleteMapping("/book/{id}")
+    public Boolean delete(@PathVariable("id") String id) {
+        return bookService.delete(id);
+    }
+
+    @PutMapping("/book/{id}")
+    public Boolean update(@PathVariable("id") String id, @RequestBody BookModel book) {
+        return bookService.update(id, book);
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/BookService.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Service
+public class BookService {
+    private List<BookModel> bookList = new LinkedList<>();
+
+    public BookService() {
+        BookModel book1 = new BookModel(UUID.randomUUID().toString(), "Tail of Naruto Uzumaki", "Jiraya the Gallant");
+        BookModel book2 = new BookModel(UUID.randomUUID().toString(), "Tail of Jiraya the Gallant", "Jiraya the Gallant");
+        bookList.add(book1);
+        bookList.add(book2);
+    }
+
+    public List<BookModel> getBookList() {
+        return bookList;
+    }
+
+    public BookModel getById(String id) {
+        for (BookModel b : bookList) {
+            if (Objects.equals(b.getId(), id)) {
+                return b;
+            }
+        }
+        return null;
+    }
+
+    public Boolean delete(String id) {
+        return bookList.removeIf(bookModel -> bookModel.getId().equals(id));
+    }
+
+    public Boolean update(String id, BookModel book) {
+        for (BookModel b : bookList) {
+            if (Objects.equals(b.getId(), id)) {
+                b.setName(book.getName());
+                b.setAuthor(book.getAuthor());
+                return true;
+            }
+        }
+        return false;
+    }
+}
